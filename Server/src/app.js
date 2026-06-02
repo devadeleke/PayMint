c1972cb972d125ce07bdfbda1c3a4fa92d375b4a
aov <victoryolawumi@gmail.com>
2026-06-02 16:42:41 +0100

    feat(auth): add a auth middleware that checks if user is authenticated

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import authRoutes from './routes/auth.route.js';
 import {errorMiddleware} from "./middlewares/error.middleware.js";
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(helmet())
 app.use(express.json())
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRoutes)
 
--- Server/src/routes/auth.route.js
+++ Server/src/routes/auth.route.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { signup, login, logout, verifyEmail, forgotPassword, resetPassword } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
+router.get('/check-auth', protectRoute, (req, res) => res.status(200).json(req.user));
 router.post('/signup', signup)
 router.post('/login', login)
 router.post('/verify-email', verifyEmail)
